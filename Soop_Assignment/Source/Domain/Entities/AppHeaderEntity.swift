e2adee3ad61dc6771832011f1bdb31ed3771665f
Kim junhwan <tazanboy1@naver.com>
2024-04-20 22:21:33 +0900

    [chore] AppHeaderEntity Foundation import 제거

--- Soop_Assignment/Source/Domain/Entities/AppHeaderEntity.swift
+++ Soop_Assignment/Source/Domain/Entities/AppHeaderEntity.swift
@@ -5,8 +5,6 @@
 //  Created by JunHwan Kim on 2024/04/20.
 //
 
-import Foundation
-
 struct AppHeaderEntity {
     let appName: String
     let appIconImagePath: String
---
+++ Soop_Assignment/Source/Domain/Entities/AppRatingEntity.swift
@@ -0,0 +1,8 @@
+//
+//  AppRatingEntity.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/20.
+//
+
+import Foundation
