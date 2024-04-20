6763dcf9f1e717ca7a3411d728deb917121a7d68
Kim junhwan <tazanboy1@naver.com>
2024-04-20 21:29:27 +0900

    Initial Commit

---
+++ Soop_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
