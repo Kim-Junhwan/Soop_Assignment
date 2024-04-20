b7fa50a0df6398e213dde92b62dd4130d5cf3562
Kim junhwan <tazanboy1@naver.com>
2024-04-21 02:36:08 +0900

    [feat] DetailInfo 정의

---
+++ Soop_Assignment/Source/Domain/Entities/DetailInfoEntity.swift
@@ -0,0 +1,13 @@
+//
+//  DetailInfoEntity.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/21.
+//
+
+struct DetailInfoEntity {
+    let headerInfo: AppHeaderEntity
+    let ratingInfo: AppRatingEntity
+    let subInfo: AppSubInfoEntity
+    
+}
