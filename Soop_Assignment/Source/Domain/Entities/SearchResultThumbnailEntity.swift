bc70ffff2d4f46ab3490061afdb9da8b6d6e3ea4
Kim junhwan <tazanboy1@naver.com>
2024-04-20 22:35:42 +0900

    [feat] 검색결과 Entity 정의

---
+++ Soop_Assignment/Source/Domain/Entities/SearchResultThumbnailEntity.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultThumbnailEntity.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/20.
+//
+
+struct SearchResultThumbnailEntity {
+    let headerInfo: AppHeaderEntity
+    let ratingInfo: AppRatingEntity
+    let subInfo: AppSubInfoEntity
+}
