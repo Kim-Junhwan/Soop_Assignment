8c16f33d1580c9a3ffd3866e693d78f9e17aa7d9
Kim junhwan <tazanboy1@naver.com>
2024-04-20 22:28:19 +0900

    [feat] AppSubInfoEntity 정의

---
+++ Soop_Assignment/Source/Domain/Entities/AppSubInfoEntity.swift
@@ -0,0 +1,12 @@
+//
+//  AppSubInfoEntity.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/20.
+//
+
+struct AppSubInfoEntity {
+    let appThumbnailImagePath: [String]
+    let genre: String
+    let developerName: String
+}
