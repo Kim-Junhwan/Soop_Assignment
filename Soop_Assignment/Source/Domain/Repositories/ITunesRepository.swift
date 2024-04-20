7cdd0af795f5c32115c055cd36f2c36c5dc66c64
Kim junhwan <tazanboy1@naver.com>
2024-04-21 00:31:36 +0900

    [feat] ITunesRepository 정의

---
+++ Soop_Assignment/Source/Domain/Repositories/ITunesRepository.swift
@@ -0,0 +1,12 @@
+//
+//  ITunesRepository.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/21.
+//
+
+import RxSwift
+
+protocol ITunesRepository {
+    func fetchSearchResultList() -> Single<[SearchResultThumbnailEntity]>
+}
