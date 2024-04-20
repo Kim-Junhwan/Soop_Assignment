c41fd43f1bffee7236e9ce2d0966198213b47daf
Kim junhwan <tazanboy1@naver.com>
2024-04-21 01:43:25 +0900

    [feat] FetchDetailInfoQuery 정의

---
+++ Soop_Assignment/Source/Domain/Entities/Query/FetchDetailInfoQuery.swift
@@ -0,0 +1,12 @@
+//
+//  FetchDetailInfoQuery.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/21.
+//
+
+import Foundation
+
+struct FetchDetailInfoQuery {
+    let appId: Int
+}
