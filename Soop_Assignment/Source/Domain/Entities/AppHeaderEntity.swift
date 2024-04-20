8e36eb378aef3b21f17408bc33ca3cba38fee481
Kim junhwan <tazanboy1@naver.com>
2024-04-20 22:16:47 +0900

    [feat] AppHeaderEntity 정의

---
+++ Soop_Assignment/Source/Domain/Entities/AppHeaderEntity.swift
@@ -0,0 +1,13 @@
+//
+//  AppHeaderEntity.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/20.
+//
+
+import Foundation
+
+struct AppHeaderEntity {
+    let appName: String
+    let appIconImagePath: String
+}
