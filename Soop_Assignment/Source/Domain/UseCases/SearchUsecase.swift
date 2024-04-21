9afc3548b4a80026e554e04fbcccd1048eab93f7
Kim junhwan <tazanboy1@naver.com>
2024-04-21 14:46:28 +0900

    [feat] SearchUsecase에서 Single을 create하도록 구현

--- Soop_Assignment/Source/Domain/UseCases/SearchUsecase.swift
+++ Soop_Assignment/Source/Domain/UseCases/SearchUsecase.swift
@@ -10,3 +10,32 @@ import RxSwift
 protocol SearchUsecase {
     func excute(word: String, offset: Int) -> Single<[SearchResultThumbnailEntity]>
 }
+
+final class DefaultSearchUsecase {
+    private let itunesRepository: ITunesRepository
+    
+    init(itunesRepository: ITunesRepository) {
+        self.itunesRepository = itunesRepository
+    }
+}
+
+extension DefaultSearchUsecase: SearchUsecase {
+    
+    func excute(word: String, offset: Int) -> Single<[SearchResultThumbnailEntity]> {
+        let query = SearchQuery(searchWord: word, offset: offset)
+        return Single.create { single in
+            let task = self.itunesRepository.fetchSearchResultList(query: query) { result in
+                switch result {
+                case .success(let searchResult):
+                    single(.success(searchResult))
+                case .failure(let error):
+                    single(.failure(error))
+                }
+            }
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+    
+}
