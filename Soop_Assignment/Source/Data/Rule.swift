f35516d461f1123bd261831f5f9fdbd38611c8dd
Kim junhwan <tazanboy1@naver.com>
2024-04-24 00:21:44 +0900

    [feat] 검색 규칙 규정한 Rule 정의

---
+++ Soop_Assignment/Source/Data/Rule.swift
@@ -0,0 +1,15 @@
+//
+//  Rule.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/24.
+//
+
+import Foundation
+
+enum Rule {
+    static let fetchCount: Int = 20
+    static let language: String = "ko_KR"
+    static let media: String = "software"
+    static let country: String = "KR"
+}
