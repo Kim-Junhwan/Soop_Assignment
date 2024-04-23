73f3efdfd7f33a749ee7a058b3131bb90913ee8a
Kim junhwan <tazanboy1@naver.com>
2024-04-24 03:35:51 +0900

    [feat] SearchRequestDTO에 기본 쿼리문 추가

--- Soop_Assignment/Source/Data/RequestDTO/SearchRequestDTO.swift
+++ Soop_Assignment/Source/Data/RequestDTO/SearchRequestDTO.swift
@@ -10,4 +10,8 @@ import Foundation
 struct SearchRequestDTO: Encodable {
     let term: String
     let offset: Int
+    let country: String = Rule.country
+    let media: String = Rule.media
+    let language: String = Rule.language
+    let limit: Int = Rule.fetchCount
 }
