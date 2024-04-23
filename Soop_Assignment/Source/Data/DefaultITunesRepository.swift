d00965da7ffbf7ca78d91f614ba9ca8fc7764ef7
Kim junhwan <tazanboy1@naver.com>
2024-04-24 04:13:54 +0900

    [feat] DefaultITunesRepository 프로토콜 채택 및 기능 구현

--- Soop_Assignment/Source/Data/DefaultITunesRepository.swift
+++ Soop_Assignment/Source/Data/DefaultITunesRepository.swift
@@ -15,4 +15,35 @@ final class DefaultITunesRepository {
     }
 }
 
-
+extension DefaultITunesRepository: ITunesRepository {
+    func fetchSearchResultList(query: SearchQuery, completion: @escaping (Result<[SearchResultThumbnailEntity], Error>) -> Void) -> Cancellable? {
+        let requestDTO = SearchRequestDTO(term: query.searchWord, offset: query.offset)
+        return dataTransferService.request(endPoint: ITunesRouter.search(requestDTO)) { (result: Result<SearchResponseDTO, Error>) in
+            switch result {
+            case .success(let searchResult):
+                completion(.success(searchResult.toDomain()))
+            case .failure(let failure):
+                completion(.failure(failure))
+            }
+        }
+    }
+    
+    func fetchDetailInfo(query: FetchDetailInfoQuery, completion: @escaping (Result<DetailInfoEntity, Error>) -> Void) -> Cancellable? {
+        let requestDTO = FetchDetailInfoRequestDTO(id: query.appId)
+        return dataTransferService.request(endPoint: ITunesRouter.fetchDetailInfo(requestDTO)) { (result: Result<DetailResponseDTO, Error>) in
+            switch result {
+            case .success(let searchResult):
+                do {
+                    let entity = try searchResult.toDomain()
+                    completion(.success(entity))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let failure):
+                completion(.failure(failure))
+            }
+        }
+    }
+    
+    
+}
