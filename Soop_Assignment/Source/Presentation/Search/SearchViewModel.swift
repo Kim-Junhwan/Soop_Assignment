540519e77225f8b9cab2507a20c0c245e0a150a2
Kim junhwan <tazanboy1@naver.com>
2024-04-24 06:49:58 +0900

    [feat] SearchViewModel 에러 Output 추가

--- Soop_Assignment/Source/Presentation/Search/SearchViewModel.swift
+++ Soop_Assignment/Source/Presentation/Search/SearchViewModel.swift
@@ -13,16 +13,19 @@ final class SearchViewModel: ViewModelBase {
     
     struct Input {
         let search: Signal<String>
+        let tapCancelButton: ControlEvent<Void>
     }
     
     struct Output {
         let searchResult: Signal<[SearchThumbnailModel]>
         let isLoading: Signal<Bool>
+        let currentError: Signal<Error>
     }
     
     private let searchUsecase: SearchUsecase
     private let isLoading: PublishRelay<Bool> = .init()
     private let searchResults: PublishRelay<[SearchThumbnailModel]> = .init()
+    private let currentError: PublishRelay<Error> = .init()
     var disposeBag: DisposeBag = .init()
     
     init(searchUsecase: SearchUsecase) {
@@ -30,23 +33,30 @@ final class SearchViewModel: ViewModelBase {
     }
     
     func transform(input: Input) -> Output {
-        input.search
+        let searchResult = input.search
             .do(onNext: { [weak self] _ in
                 self?.isLoading.accept(true)
             })
             .flatMap { word in
-                self.searchUsecase.excute(word: word, offset: 0).asSignal(onErrorJustReturn: [])
+                self.searchUsecase.excute(word: word, offset: 0)
+                    .do(onError: { [weak self] error in
+                        self?.currentError.accept(error)
+                    })
+                    .asSignal(onErrorJustReturn: [])
             }
             .map { result in
                 result.map { SearchThumbnailModel(thumbnailEntity: $0) }
             }
-            .emit(with: self) { owner, result in
-                owner.isLoading.accept(false)
-                owner.searchResults.accept(result)
+            .do(onNext: { [weak self] _ in
+                self?.isLoading.accept(false)
+            })
+            
+        input.tapCancelButton
+            .subscribe(with: self) { owner, _ in
+                owner.searchResults.accept([])
             }
             .disposed(by: disposeBag)
-            
         
-        return .init(searchResult: searchResults.asSignal(), isLoading: isLoading.asSignal())
+        return .init(searchResult: searchResult.asSignal(), isLoading: isLoading.asSignal(), currentError: currentError.asSignal())
     }
 }
