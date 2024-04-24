336e705f8bb43894e9186eb5eb0a91b5dc63dde9
Kim junhwan <tazanboy1@naver.com>
2024-04-24 15:18:12 +0900

    [feat] DetailViewController 기본적인 프로퍼티 구현

--- Soop_Assignment/Source/Presentation/Detail/DetailViewController.swift
+++ Soop_Assignment/Source/Presentation/Detail/DetailViewController.swift
@@ -8,11 +8,26 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    private let viewModel: DetailViewModel
+    private let detailView = DetailView(frame: .zero)
+    
+    init(viewModel: DetailViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = detailView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.largeTitleDisplayMode = .never
     }
     
 
