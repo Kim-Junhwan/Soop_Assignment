582142a6129c65866d52e03b8d7e6f13ca4f62d7
Kim junhwan <tazanboy1@naver.com>
2024-04-24 13:50:00 +0900

    [feat] UIImageView에 이미지 가져와서 적용하는 기능 추가

---
+++ Soop_Assignment/Source/Presentation/Extensions/UIImageView+.swift
@@ -0,0 +1,36 @@
+//
+//  UIImageView+.swift
+//  Soop_Assignment
+//
+//  Created by JunHwan Kim on 2024/04/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func setImageFromImagePath(imagePath: String) {
+        if let image = ImageCacheManager.shared.checkProfileImageInCache(imageURL: imagePath) {
+            self.image = image
+            return
+        }
+        guard let url = URL(string: imagePath) else { return }
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.timeoutIntervalForRequest = 10.0
+        let session = URLSession(configuration: sessionConfig)
+        let request = URLRequest(url: url)
+        session.dataTask(with: request) { data, response, error in
+            let fetchImage: UIImage?
+            if let error = error {
+                fetchImage = UIImage(systemName: "xmark")
+            } else {
+                guard let data, let response, let safeImage = UIImage(data: data) else { return }
+                fetchImage = UIImage(data: data)
+                DispatchQueue.main.async {
+                    ImageCacheManager.shared.cache.setObject(safeImage, forKey: imagePath as NSString)
+                    self.image = fetchImage
+                }
+            }
+        }.resume()
+    }
+}
